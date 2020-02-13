ce56e6b3fd4a0c95655baa2383253dbd0b45a97c
sendo <abcy.ns.gbd.9st@gmail.com>
2020-02-13 15:59:25 +0900

    modify test method

--- haw/haw_book_manage/tests.py
+++ haw/haw_book_manage/tests.py
@@ -1,6 +1,20 @@
 from django.urls import reverse
 from django.test import TestCase
 from django.utils import timezone
+from .models import TblBook
+
+
+class CreateTestData:
+    # テストデータの作成
+    def data_create(self):
+        TblBook.objects.create(
+            title='はじめての自動化',
+            author='ハロー、オートメーションワールド！',
+            publisher='ハロー、オートメーションワールド！',
+            finished_date='2020-01-01',
+            created_at=timezone.now(),
+            updated_at=timezone.now()
+        )
 
 
 class BookTopTest(TestCase):
@@ -32,21 +46,29 @@ class BookCreateTest(TestCase):
 class BookUpdateTest(TestCase):
     # データ更新テスト
     def test_update(self):
+        # テストデータを作成
+        create_test_data = CreateTestData()
+        create_test_data.data_create()
+
         update_data = {
             'title': 'はじめての自動化 第二版',
             'author': 'Hello, Automation World!',
         }
         res = self.client.post(
-            reverse('haw:update', kwargs={'pk': 1}),
+            reverse('haw:update', kwargs={'book_id': 1}),
             data=update_data
         )
-        self.assertEqual(res.status_code, 302)
+        self.assertEqual(res.status_code, 200)
 
 
 class BookDeleteTest(TestCase):
     # データ削除テスト
     def test_delete(self):
+        # テストデータを作成
+        create_test_data = CreateTestData()
+        create_test_data.data_create()
+
         res = self.client.post(
-            reverse('haw:delete', kwargs={'pk': 1})
+            reverse('haw:delete', kwargs={'book_id': 1})
         )
         self.assertEqual(res.status_code, 302)
