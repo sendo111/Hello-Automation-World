7c5018f54dcc208fe73384862ed4f8da971f6a9a
sendo <abcy.ns.gbd.9st@gmail.com>
2020-02-16 18:43:20 +0900

    modify update method

--- haw/haw_book_manage/views/update.py
+++ haw/haw_book_manage/views/update.py
@@ -16,7 +16,7 @@ def update(request, book_id):
             'finished_date'
         ).get(id=book_id)
         return render(request, template_name, bt)
-    else:
+    elif request.method == 'POST':
         update_bt = Book.objects.get(id=book_id)
         update_bt.title = request.POST['title']
         update_bt.author = request.POST['author']
