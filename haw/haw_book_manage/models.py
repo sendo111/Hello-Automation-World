d244e6647f0881368face7c58c83e2ff0e9c0255
sendo <abcy.ns.gbd.9st@gmail.com>
2020-02-16 14:17:15 +0900

    form reject space

--- haw/haw_book_manage/models.py
+++ haw/haw_book_manage/models.py
@@ -3,12 +3,12 @@ from django.utils import timezone
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=100, null=False, default='')
-    author = models.CharField(max_length=50, null=False, default='')
-    publisher = models.CharField(max_length=50, null=False, default='')
-    finished_date = models.DateField(default=timezone.now, null=False)
-    created_at = models.DateTimeField(default=timezone.now, null=False)
-    updated_at = models.DateTimeField(default=timezone.now, null=False)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    publisher = models.CharField(max_length=50)
+    finished_date = models.DateField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
